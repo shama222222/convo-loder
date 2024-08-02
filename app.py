30bbd022a025071d9d0a9da9a7a505ee588431d9
hassan3828 <hassan38281@gmail.com>
2024-08-02 23:24:27 +0500

    app.py

--- app.py
+++ app.py
@@ -145,4 +145,6 @@ def home():
     return render_template_string(html_content)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
